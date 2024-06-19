925e4012e2f6b3d4391e67bbedbb81c3543a8bba
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2024-06-19 00:36:23 -0300

    Resolvendo Desafio 10
    Signed-off-by: Felipe-Netto <felipeg.alvesnetto@gmail.com>

---
+++ Desafios_Stream_API/src/Desafio_10.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio_10 {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        /*
+         Desafio 10 - Agrupe os valores ímpares múltiplos de 3 ou de 5:
+         Utilize a Stream API para agrupar os valores ímpares múltiplos
+         de 3 ou de 5 e exiba o resultado no console.
+        */
+
+        List<Integer> valoresImparesMultiplosDeTresOuCinco = numeros.stream()
+                .filter(n -> ((n % 2 != 0) && ((n % 3 == 0) || (n % 5 == 0))))
+                .toList();
+
+        System.out.println("Numeros ímpares multiplos de 3 ou 5");
+        valoresImparesMultiplosDeTresOuCinco.forEach(System.out::println);
+    }
+
+}
